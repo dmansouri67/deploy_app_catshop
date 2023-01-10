2c4f8a5a7d44562127bdb07e23915aeda2ff5bff
Dalila MANSOURI <dmansouri396@gmail.com>
2023-01-10 18:35:33 +0100

    deploy app

--- deploy/bootstrap.php
+++ deploy/bootstrap.php
@@ -1,16 +1,58 @@
 <?php
+
+require_once "vendor/autoload.php";
 use Doctrine\ORM\Tools\Setup;
 use Doctrine\ORM\EntityManager;
-date_default_timezone_set('America/Lima');
-require_once "vendor/autoload.php";
-$isDevMode = true;
-$config = Setup::createYAMLMetadataConfiguration(array(__DIR__ . "/config/yaml"), $isDevMode);
-$conn = array(
-'host' => 'host',
-'driver' => 'pdo_pgsql',
-'user' => 'cnam_user',
-'password' => 'pass',
-'dbname' => 'cnam',
-'port' => '5432'
-);
-$entityManager = EntityManager::create($conn, $config);
+
+date_default_timezone_set('Europe/Paris');
+const JWT_SECRET = "makey1234567";
+
+class Config {
+    private static ?Config $instance = null; //peut être null
+    public ?EntityManager $entityManager = null;
+    public ?Mixed $options = null;
+
+    private function _construct()
+    {
+        $isDevMode = true;
+        $config = Setup::createYAMLMetadataConfiguration(array(__DIR__ . "/config/yaml"), $isDevMode);
+        $conn = array(
+            'host' => '127.0.0.1',
+            'driver' => 'pdo_pgsql',
+            'user' => 'root',
+            'password' => '',
+            'dbname' => 'cat\'shop_db',
+            'port' => '3306'
+        );
+        $this->entityManager = EntityManager::create($conn, $config);
+
+        $this->options = [
+            "attribute" => "token",
+            "header" => "Authorization",
+            "regexp" => "/Bearer\s+(.*)$/i",
+            "secure" => false,
+            "algorithm" => ["HS256"],
+            "secret" => JWT_SECRET,
+            "path" => ["/api"],
+            "ignore" => ["/api/login", "/api/register"],
+            "error" => function ($response, $arguments) {
+                $data["status"] = "error";
+                $data["message"] = $arguments["message"];
+                return $response
+                    ->withHeader("Content-Type", "application/json")
+                    ->getBody()->write(json_encode($data, JSON_UNESCAPED_SLASHES));
+            }
+        ];
+    }
+
+    public static function getInstance() : Config {
+        if (self::$instance == null) {
+            self::$instance = new Config();
+        }
+        return self::$instance;
+    }
+
+    public function getEntityManager() : EntityManager {
+        return $this->entityManager;
+    }
+}
--- deploy/cli-config.php
+++ deploy/cli-config.php
@@ -1,4 +1,9 @@
 <?php
 use Doctrine\ORM\Tools\Console\ConsoleRunner;
 require_once 'bootstrap.php';
-return ConsoleRunner::createHelperSet($entityManager);
+$helperSet = new \Symfony\Component\Console\Helper\HelperSet(array(
+    'db' => new \Doctrine\DBAL\Tools\Console\Helper\ConnectionHelper($em->getConnection()),
+    'em' => new \Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper($em)
+));
+return $helperSet;
+// return ConsoleRunner::createHelperSet(Config::getInstance()->entityManager);
